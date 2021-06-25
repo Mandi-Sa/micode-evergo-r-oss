771d03bfc0d4bf761e4d00cd765e1e9ff89d3768
jiangyanjun <jiangyanjun@wingtech.com>
2021-06-25 14:03:26 +0800

    POWER: [thermal] commit MTK patch(aptional) for default param
    Jira ID: HONGMI-84846
    
    Change-Id: I6ad2e7b9007599acdee288872e65cae881ac6c86
    Signed-off-by: jiangyanjun <jiangyanjun@wingtech.com>

--- drivers/misc/mediatek/thermal/common/thermal_zones/mtk_ts_btsnrpa.c
+++ drivers/misc/mediatek/thermal/common/thermal_zones/mtk_ts_btsnrpa.c
@@ -71,8 +71,10 @@ static int mtkts_btsnrpa_debug_log;
 static int kernelmode;
 static int g_THERMAL_TRIP[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
-static int num_trip;
-static char g_bind0[20] = {"mtk-cl-shutdown03"};
+/* BSP.Charge - 2020.12.11 - enable first trip temp and blind cooler - start */
+static int num_trip = 1;
+static char g_bind0[20] = "mtk-cl-kshutdown01";
+/* BSP.Charge - 2020.12.11 - enable first trip temp and blind cooler - end */
 static char g_bind1[20] = { 0 };
 static char g_bind2[20] = { 0 };
 static char g_bind3[20] = { 0 };
