4e7ab1a6a886b42419b399ef5da58585fe485d2a
chenrui1 <chenrui1@wingtech.com>
2021-06-28 19:48:30 +0800

    [charger]{type:modify,pro:K16A} modify decimal soc
    [WT] [HONGMI 84841]
    
    [comment]: show decimal soc when charge type is super charge
    Signed-off-by: chenrui1 <chenrui1@wingtech.com>
    
    Change-Id: I821811cf314ce0fed1b15307764dfb7afe4d4b62

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -73,7 +73,7 @@ static const char * const power_supply_usb_typec_mode_text[] = {
 
 //Extb HONGMI-84869,wangbin wt.ADD,20210616,add charge type
 static const char * const power_supply_charge_type_text[] = {
-	"Unknown", "N/A", "Trickle", "Fast", "Taper"
+	"Unknown", "N/A", "Trickle", "Taper", "Fast"
 };
 
 static const char * const power_supply_health_text[] = {
