1af1c5ff6be273905acc90f56a95af87e23e762c
p-heyutong <he_yutong@hoperun.com>
2021-07-17 14:00:56 +0800

    Camera:everygreen:BSP:修改冷热启动不达标问题[1/1]
    HONGMI-86990
    
    1、修改camera驱动的读写方式，i2c speed改为1000
    2、修改冷热启动不达标问题
    Signed-off-by: he_yutong<he_yutong@hoperun.com>
    
    Change-Id: I67c4a9c6892163ba72db2684102682a8c8bb3777

--- drivers/misc/mediatek/imgsensor/src/common/v1_1/s5kjn1sunny_mipi_raw/s5kjn1sunny_mipi_raw.h
+++ drivers/misc/mediatek/imgsensor/src/common/v1_1/s5kjn1sunny_mipi_raw/s5kjn1sunny_mipi_raw.h
@@ -153,7 +153,12 @@ struct imgsensor_info_struct {
 extern int iReadRegI2C(u8 *a_pSendData, u16 a_sizeSendData,
 	u8 *a_pRecvData, u16 a_sizeRecvData,
 		       u16 i2cId);
+extern int iReadRegI2CTiming(u8 *a_pSendData, u16 a_sizeSendData,
+		u8 *a_pRecvData, u16 a_sizeRecvData, u16 i2cId, u16 timing);
+
 extern int iWriteRegI2C(u8 *a_pSendData, u16 a_sizeSendData, u16 i2cId);
+extern int iWriteRegI2CTiming(u8 *a_pSendData, u16 a_sizeSendData,
+		u16 i2cId, u16 timing);
 
 extern void read_imx230_eeprom(void);
 int iBurstWriteReg_multi(u8 *pData, u32 bytes, u16 i2cId,
