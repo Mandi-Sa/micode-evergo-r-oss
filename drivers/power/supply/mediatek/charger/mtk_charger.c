de47fd42c82c73f44052d5839d07afd288543d08
caijiaqi <caijiaqi@wingtech.com>
2021-08-10 16:23:46 +0800

    [K16A][charger] ATO version control charging
    [XIAOMI] HONGMI-93798
    
    [comment] ATO version control charging
    
    Change-Id: Ib054cfda95e3997792ba0fab58bd6d207f721009
    Signed-off-by: caijiaqi <caijiaqi@wingtech.com>

--- drivers/power/supply/mediatek/charger/mtk_charger.c
+++ drivers/power/supply/mediatek/charger/mtk_charger.c
@@ -2318,7 +2318,7 @@ static int charger_routine_thread(void *arg)
 
 //+Bug653711, chenrui1.wt,ADD,20210520,add control charging capacity
 #ifdef WT_COMPILE_FACTORY_VERSION
-		if (is_charger_on) {
+		if (VChr > 4500) {
 			mtk_charging_control(info);
 		}
 #endif
